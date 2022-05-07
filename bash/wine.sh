d48fcca07d27b0a9eaad4d74015c09959229e9a1
Mobin Aydinfar <85447245+mobin-2008@users.noreply.github.com>
2022-05-08 01:06:59 +0430

    Adding license info & updating wine.sh
    Sigend-off-by: Mobin Aydinfar <mobin@mobintestserver.ir>

--- bash/wine.sh
+++ bash/wine.sh
@@ -2,6 +2,7 @@
 # Script for downloading & installing wine on Debian based systems
 # Originaly author: #ToDo: please write your information
 # Modified by: Mobin Aydinfar <mobin@mobintestserver.ir>
+# License: GPLv3+
 # Project: https://github.com/qasem-talaee/wine-pyqt5
 
 # Updating repos info using apt
@@ -30,16 +31,53 @@ fi
 # 1. Using wine in official repos, its default & usually there is a very old version of wine in it.
 # 2. Using wine repo on ubuntu/debian. for this, you must uncomment its & commnet #1 installing commands
 
-# ToDo: Using fail detecter
-# 2:
-#wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-#sudo mv winehq-jammy.sources /etc/apt/sources.list.d/
-#wget -nc https://dl.winehq.org/wine-builds/winehq.key
-#sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
-#sudo apt update
-#sudo apt install winehq-staging --install-recommends
+# 2(Advanced):
 
-# 1 (default):
+# Downloading wine repo info for apt
+# ToDo: Find a way to detect distro & auto setting $1 & $2
+# $1 = ubuntu  ## Distro name (ubuntu, debian, devuan &...)
+# S2 = jammy ## Distro code name (jammy, bullseye, ceres & ...)
+#wget -nc https://dl.winehq.org/wine-builds/$1/dists/$2/winehq-$2.sources
+#if [ $? -ne 0 ]  ; then
+#  #echo "[INFO] Downloading wine repo info is Done!"
+#  exit 0
+#fi
+
+# Adding wine repo info to apt sources
+#sudo mv winehq-$2.sources /etc/apt/sources.list.d/
+#if [ $? -ne 0 ]  ; then
+#  #echo "[INFO] Adding wine repo info to apt sources is Done!"
+#  exit 0
+#fi
+
+# Downloading winehq keyring for verify packages from wine repo
+#wget -nc https://dl.winehq.org/wine-builds/winehq.key
+#if [ $? -ne 0 ]  ; then
+#  #echo "[INFO] Downloading winehq keyring is Done!"
+#  exit 0
+#fi
+
+# Adding winehq.key to keyrings
+# apt-key is Deprecated. copyring [file].key to /etc/share/keyrings/ to adding keyring on apt
+#sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
+#if [ $? -ne 0 ]  ; then
+#  #echo "[INFO] Adding winehq keyring is Done!"
+#  exit 0
+#fi
+
+# Updating repo info
+#sudo apt update
+#if [ $? -ne 0 ]  ; then
+#  #echo "[INFO] apt-update is Done!"
+#  exit 0
+#fi
+
+# Installing winehq-[staging or devel or stable]
+# By Default, installing winehq-staging
+# $3 = staging
+#sudo apt install winehq-$3 --install-recommends
+
+# 1 (Default):
 sudo apt install --install-recommends wine-stable -y
 if [ $? -ne 0 ]  ; then
   #echo "[INFO] Installing wine is Done!"
